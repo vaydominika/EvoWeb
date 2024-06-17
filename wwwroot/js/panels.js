29cd815d8b3056798379dfff4fd0d25bea68cd29
Dominika <vaydominika@gmail.com>
2024-06-17 09:27:32 +0200

    design update

---
+++ wwwroot/js/panels.js
@@ -0,0 +1,15 @@
+﻿function updateNotificationCount(count) {
+    var notificationBadge = document.getElementById('notificationcounter');
+    if (count > 10) {
+        notificationBadge.textContent = '10+';
+    }
+    else if (count == 0) {
+        notificationBadge.className = "d-none";
+    }
+    else {
+        notificationBadge.textContent = count;
+    }
+}
+
+// Teszt: Értesítések számának frissítése
+updateNotificationCount(12); // Próbáld ki különböző értékekkelkülönböző értékekkel
