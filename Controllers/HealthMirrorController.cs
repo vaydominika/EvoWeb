212adc27b70ffea7f215f6697b5ba9b41fd41f6b
matyas-toth <matyastoth.mail@gmail.com>
2024-06-17 08:14:21 +0200

    Updates

---
+++ Controllers/HealthMirrorController.cs
@@ -0,0 +1,31 @@
+﻿using EvoWeb.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvoWeb.Controllers
+{
+    [Route("apimirror/v1/[controller]")]
+    [ApiController]
+    public class HealthMirrorController : ControllerBase
+    {
+
+        public async Task<GenericResponse> Index()
+        {
+            HealthRequest checkprofile = new HealthRequest();
+            await checkprofile.GetDataFromApi();
+
+            GenericResponse result = checkprofile.GetResult();
+
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return new GenericResponse() { error = true, response = "backend-failure" };
+            }
+
+        }
+
+
+    }
+}
--- Core/HealthRequest.cs
+++ Core/HealthRequest.cs
@@ -1,12 +1,50 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace EvoWeb.Core
 {
-    public class HealthRequest : Controller
+    public class HealthRequest
     {
-        public IActionResult Index()
+        public GenericResponse result;
+        public async Task<ActionResult> GetDataFromApi()
         {
-            return View();
+            // Példa API URL
+            string apiUrl = "https://evoweb-api.reigdev.hu/api/v1/Health";
+
+            try
+            {
+                // HTTP kliens létrehozása
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    // API hívása és válasz visszakapása
+                    HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                    // Ha a válasz sikeres
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // JSON válasz kiolvasása
+                        string jsonResult = await response.Content.ReadAsStringAsync();
+
+                        GenericResponse responseObject = JsonConvert.DeserializeObject<GenericResponse>(jsonResult);
+
+                        // JSON válasz visszaadása a nézetnek
+                        result = responseObject;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (HttpRequestException)
+            {
+
+            }
+
+            return null;
         }
+
+
+        public GenericResponse GetResult() { return result; }
     }
 }
