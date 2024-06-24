39b968206d4a3d3a437a611e2d7a719d730a1841
matyas-toth <matyastoth.mail@gmail.com>
2024-06-24 09:06:11 +0200

    Likes

---
+++ Controllers/RevokeLikeMirrorController.cs
@@ -0,0 +1,31 @@
+﻿using EvoWeb.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvoWeb.Controllers
+{
+    [Route("apimirror/v1/[controller]")]
+    [ApiController]
+    public class RevokeLikeMirrorController : ControllerBase
+    {
+
+        public async Task<GenericResponse> Index(string sessionid, int postid)
+        {
+            RevokeLikeRequest checkprofile = new RevokeLikeRequest();
+            await checkprofile.GetDataFromApi(sessionid, postid);
+
+            GenericResponse result = checkprofile.GetResult();
+
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                return new GenericResponse() { error = true, response = "backend-failure" };
+            }
+
+        }
+
+
+    }
+}
